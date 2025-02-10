e52c01c8aff210c6f54adc947cb9a81e311aa115
ricky-kiva <ricky.kiva@gmail.com>
2025-02-10 21:51:54 +0700

    feat(index): impl. `/api/patients` endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import diagnoseRouter from './routes/diagnoseRoutes';
+import patientRouter from './routes/patientRoutes';
 
 const PORT = 3001;
 const app = express();
@@ -8,11 +9,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/diagnoses', diagnoseRouter);
+app.use('/api/patients', patientRouter);
+
 app.get('/api/ping', (_req, res) => {
   res.send('pong!');
 });
-
-app.use('/api/diagnoses', diagnoseRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
