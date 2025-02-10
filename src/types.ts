2f65604754fa53968f524b4e4036413a39111412
ricky-kiva <ricky.kiva@gmail.com>
2025-02-10 21:50:33 +0700

    feat(types): add `Patient` interface & its Omit type

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,16 @@
+export type PatientWithoutSSN = Omit<Patient, 'ssn'>;
+
 export interface Diagnosis {
   code: string,
   name: string,
   latin?: string
+}
+
+export interface Patient {
+  id: string,
+  name: string,
+  dateOfBirth: string,
+  ssn: string,
+  gender: string,
+  occupation: string
 }
