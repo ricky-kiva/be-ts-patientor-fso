694519af72d04c5dfc0d4acb51a4c44d77b8cb87
ricky-kiva <ricky.kiva@gmail.com>
2025-02-13 14:07:41 +0700

    feat(): change `Patient`'s `gender` field to Enum

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,23 @@
+export enum Gender {
+  Male = 'male',
+  Female = 'female',
+  Other = 'other'
+};
+
 export type PatientWithoutSSN = Omit<Patient, 'ssn'>;
+export type NewPatient = Omit<Patient, 'id'>;
 
 export interface Diagnosis {
   code: string,
   name: string,
   latin?: string
-}
+};
 
 export interface Patient {
   id: string,
   name: string,
   dateOfBirth: string,
   ssn: string,
-  gender: string,
+  gender: Gender,
   occupation: string
-}
+};
