5b4eb51631d4960f2f893597c089e0feba87b14a
ricky-kiva <ricky.kiva@gmail.com>
2025-02-13 14:09:23 +0700

    feat(routes): add endpoint to add new `Patient`

--- src/routes/patientRoutes.ts
+++ src/routes/patientRoutes.ts
@@ -1,11 +1,32 @@
 import express, { Response } from 'express';
 import patientServices from '../services/patientServices';
-import { PatientWithoutSSN } from '../types';
+import { NewPatient, PatientWithoutSSN } from '../types';
+import toNewPatient from '../utils';
 
 const patientRouter = express.Router();
 
 patientRouter.get('/', (_req, res: Response<PatientWithoutSSN[]>) => {
   res.send(patientServices.getPatientsWithoutSSN());
+});
+
+patientRouter.post('/', (req, res) => {
+  let newPatient: NewPatient;
+
+  try {
+    newPatient = toNewPatient(req.body);
+  } catch(e: unknown) {
+    res.status(400).json({
+      error: e instanceof Error 
+        ? e.message
+        : 'Invalid input'
+    });
+    
+    return;
+  }
+
+  const addedPatient = patientServices.addPatient(newPatient);
+
+  res.json(addedPatient);
 });
 
 export default patientRouter;
