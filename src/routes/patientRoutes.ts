a91d7ee28338cbb851a261c54612fbaf2d4ebc40
ricky-kiva <ricky.kiva@gmail.com>
2025-02-18 14:53:09 +0700

    feat(routes): impl. post new Patient Entry

--- src/routes/patientRoutes.ts
+++ src/routes/patientRoutes.ts
@@ -1,7 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express';
 import patientServices from '../services/patientServices';
-import { NewPatient, Patient, PatientWithoutSSN } from '../types';
-import { NewPatientSchema } from '../utils';
+import { Entry, NewEntry, NewPatient, Patient, PatientWithoutSSN } from '../types';
+import { NewEntrySchema, NewPatientSchema } from '../utils';
 import { z } from 'zod';
 
 const patientRouter = express.Router();
@@ -15,16 +15,17 @@ const newPatientParser = (req: Request, _res: Response, next: NextFunction) => {
   }
 };
 
+const newEntryParser = (req: Request, _res: Response, next: NextFunction) => {
+  try {
+    NewEntrySchema.parse(req.body);
+    next();
+  } catch (e: unknown) {
+    next(e);
+  }
+};
+
 patientRouter.get('/', (_req, res: Response<PatientWithoutSSN[]>) => {
   res.send(patientServices.getPatientsWithoutSSN());
-});
-
-patientRouter.post('/', newPatientParser, (
-  req: Request<unknown, unknown, NewPatient>,
-  res: Response<Patient>
-) => {
-  const addedPatient = patientServices.addPatient(req.body);
-  res.json(addedPatient);
 });
 
 patientRouter.get('/:id', (req, res: Response<Patient>) => {
@@ -36,6 +37,22 @@ patientRouter.get('/:id', (req, res: Response<Patient>) => {
   }
 
   res.send(patient);
+});
+
+patientRouter.post('/', newPatientParser, (
+  req: Request<unknown, unknown, NewPatient>,
+  res: Response<Patient>
+) => {
+  const addedPatient = patientServices.addPatient(req.body);
+  res.json(addedPatient);
+});
+
+patientRouter.post('/:id/entries', newEntryParser, (
+  req: Request<{ id: string }, unknown, NewEntry>,
+  res: Response<Entry>
+) => {
+  const addedEntry = patientServices.addEntry(req.params.id, req.body);
+  res.json(addedEntry);
 });
 
 const errorMiddleWare = (e: unknown, _req: Request, res: Response, next: NextFunction) => {
